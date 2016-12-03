e93e75ee52eff9f482a682af37a19b5d02cdb772
Fingercomp <fingercomp@users.noreply.github.com>
2016-12-03 15:53:06 +0700

    Add config module

---
+++ src/config.lua
@@ -0,0 +1,74 @@
+local fs = require("filesystem")
+
+local path = "/etc/ut-serv.conf"
+
+local function existsDir(path)
+  return fs.exists(path) and fs.isDirectory(path)
+end
+
+local function existsFile(path)
+  return fs.exists(path) and not fs.isDirectory(path)
+end
+
+local DEFAULT_CONFIG = [==[
+-- < Settings related to network >----------------------------------------------
+network = {
+  modem = {}
+}
+
+-- Modem strength
+network.modem.strength = 400
+
+-- Port to listen on and send to.
+network.modem.port = 12345
+]==]
+
+if not existsFile(path) then
+  local dirPath = fs.path(path)
+  if not existsDir(dirPath) then
+    local result, reason = fs.makeDirectory(dirPath)
+    if not result then
+      error("failed to create '" .. tostring(dirPath) .. "' directory for the config file: " .. tostring(reason))
+    end
+  end
+  local file, reason = io.open(path, "w")
+  if file then
+    file:write(DEFAULT_CONFIG)
+    file:close()
+  else
+    error("failed to open config file for writing: " .. tostring(reason))
+  end
+end
+local file, reason = io.open(path, "r")
+if not file then
+  error("failed to open config file for reading: " .. tostring(reason))
+end
+local content = file:read("*all")
+file:close()
+local globals = {}
+load(content, "config", "t", globals)()
+
+local function newUndecl(base)
+  base = base or {}
+  return setmetatable(base, {
+    __index = {
+      get = function(k, v, createNewUndecl)
+        if type(base[k]) ~= "nil" then
+          if type(base[k]) == "table" then
+            return newUndecl(base[k])
+          end
+          return base[k]
+        end
+        io.stderr:write("Attempt to access undeclared config field '" .. tostring(k) .. "'!")
+        if not createNewUndecl then
+          return v
+        else
+          return newUndecl(v)
+        end
+      end
+    }
+  })
+end
+
+local config = newUndecl(globals)
+return config
--- src/network.lua
+++ src/network.lua
@@ -11,13 +11,19 @@ local modem = component.modem
 if not modem.isWireless() then
   error("How on earth are you going to connect tablets to me? You picked the wrong modem.")
 end
-modem.setStrength(config.modem.strength)
-modem.open(config.modem.port)
+modem.setStrength(config.get("network", {}, true)
+                        .get("modem", {}, true)
+                        .get("strength", 400))
+
+local port = config.get("network", {}, true)
+                   .get("modem", {}, true)
+                   .get("port", 12345)
+modem.open(port)
 
 EventEngine:subscribe("sendmsg", events.priority.low, function(handler, evt)
   if evt.addressee then
-    modem.send(evt.addressee, config.modem.port, table.unpack(evt:get()))
+    modem.send(evt.addressee, port, table.unpack(evt:get()))
   else
-    modem.broadcast(config.modem.port, table.unpack(evt:get()))
+    modem.broadcast(port, table.unpack(evt:get()))
   end
 end)
