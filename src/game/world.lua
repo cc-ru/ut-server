4c2e2414c8b6a46a75f76be835b77bb38226685c
astral17 <2001.07.17dog@gmail.com>
2016-12-04 15:54:12 +0400

    Add and fix a lot of things in world.lua

--- src/game/world.lua
+++ src/game/world.lua
@@ -3,10 +3,12 @@
 -- Sets and unsets chests with coins.
 
 local com = require("component")
-local module = require("ut-serv.module")
+local module = require("ut-serv.modules")
 
 local db = module.load("db")
+db.blocks = {}
 local events = module.load("events")
+local config = module.load("config")
 
 local debug = com.debug
 
@@ -18,26 +20,20 @@ local coin = config.get("world", {}, true).get("item", {}, true)
 local function getBlockData(world, x, y, z)
   local id = world.getBlockId(x, y, z)
   local meta = world.getMetadata(x, y, z)
-  local nbt = world.getTileNBT(x, y, z)
-  return {id = id, meta = meta, nbt = nbt}
+  return {id = id, meta = meta}
 end
 
-local function setBlock(world, x, y, z, id, meta, nbt)
+local function setBlock(world, x, y, z, id, meta)--, nbt)
   local success, reason = world.setBlock(x, y, z, id, meta)
-  if not success then
-    return success, reason
-  end
-  local success, reason = world.setTileNBT(x, y, z, nbt)
   return success, reason
 end
 
 local function setChest(world, x, y, z)
-  local prevBlock = getBlockData(world, x, y, z)
+  local prevBlock = getBlockData(debug.getWorld(),x, y, z)
 
   local success, reason = setBlock(world, x, y, z,
                                    chest.get("id", "minecraft:chest"),
-                                   chest.get("meta", 0),
-                                   chest.get("nbt", {}))
+                                   chest.get("meta", 0))
   if not success then
     setBlock(world, x, y, z, prevBlock.id, prevBlock.meta, prevBlock.nbt)
     return success, reason
@@ -47,7 +43,8 @@ local function setChest(world, x, y, z)
     coin.get("id", "minecraft:stone"),
     math.random(table.unpack(coin.get("count", {1, 1}))),
     coin.get("meta", 0),
-    coin.get("nbt", {}),
+--    coin.get("nbt", {}),
+    "",
     x,
     y,
     z,
@@ -67,18 +64,36 @@ EventEngine:subscribe("setchest", events.priority.high, function(handler, evt)
 end)
 
 EventEngine:subscribe("unsetchest", events.priority.high, function(handler, evt)
-  local block = {}
-  for _, b in pairs(db.blocks) do
+  local block, si = {}, -1
+  for i, b in pairs(db.blocks) do
     if b.x == evt.x and b.y == evt.y and b.z == evt.z then
       block = b
+      si = i
     end
   end
   if not block then
     evt:cancel()
   end
-  local result = setBlock(debug.getWorld(), evt.x, evt.y, evt.z,
-                          block.data.id, block.data.meta, block.data.nbt)
+  if not evt.notDelete then
+    table.remove(db.blocks,si)
+  end
+  local result = setBlock(debug.getWorld(),evt.x, evt.y, evt.z, block.data.id, block.data.meta)
   if not result then
     evt:cancel()
+  end
+end)
+
+EventEngine:subscribe("onWorldTick", events.priority.high, function(handler, evt)
+  local fd = {}
+  for i = 1, #db.blocks do
+    db.blocks[i].time = db.blocks[i].time - 1
+    if db.blocks[i].time <= 0 then
+      table.insert(fd, i)
+      local ev = EventEngine:event("unsetchest")
+      EventEngine:push(ev{x = db.blocks[i].x, y = db.blocks[i].y, z = db.blocks[i].z, notDelete = true})
+    end
+  end
+  for i = #fd, 1, -1 do
+    table.remove(db.blocks, fd[i])
   end
 end)
